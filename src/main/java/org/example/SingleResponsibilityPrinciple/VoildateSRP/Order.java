1c0e4cfd58f6c572c4ed70e6f30dc52f92a04e25
chandrashekharbhaupande <chpande55@gmail.com>
2025-05-30 20:17:20 +0530

    voilating Single responsibility principle code

---
+++ src/main/java/org/example/SingleResponsibilityPrinciple/VoildateSRP/Order.java
@@ -0,0 +1,40 @@
+package org.example.SingleResponsibilityPrinciple.VoildateSRP;
+
+import org.example.SingleResponsibilityPrinciple.Item;
+
+import java.util.List;
+
+public class Order {
+    private int orderId;
+    private List<Item> items;
+
+    public Order(int orderId, List<Item> items) {
+        this.orderId = orderId;
+        this.items = items;
+    }
+
+    /*
+    1. Business logic (total calculation)
+    2. Persistence (database)
+    3. Notification (email)
+
+    That's 3 responsibilities. Violates SRP.
+
+     */
+    public double calculateTotal() {
+        return items.stream()
+                .mapToDouble(item -> item.getPrice() * item.getQuantity())
+                .sum();
+    }
+
+    public void saveToDatabase() {
+        // Simulate saving to database
+        System.out.println("Saving order to database...");
+    }
+
+    public void sendEmailConfirmation() {
+        // Simulate sending email
+        System.out.println("Sending email confirmation...");
+    }
+}
+
